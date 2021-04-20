2446da6cc300b756a371e5adb27db49ff588e580
AQ1011 <anhquocngo1011@gmail.com>
2021-04-20 21:40:23 +0700

    sign in/ sign up

---
+++ Pred/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pred.Data;
+
+[assembly: HostingStartup(typeof(Pred.Areas.Identity.IdentityHostingStartup))]
+namespace Pred.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
